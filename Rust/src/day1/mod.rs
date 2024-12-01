dac3f24cbc1aab9135954d5b2036e704bce2a806
Marcello Fabrizio <marcfabrizio10@gmail.com>
2024-12-01 11:40:17 -0300

    better practices for day1 solve 1

--- Rust/src/day1/mod.rs
+++ Rust/src/day1/mod.rs
@@ -6,23 +6,26 @@ pub fn solve1(input: &String) {
 
     let lines: Vec<&str> = input.lines().collect();
     for line in &lines {
-        let split_line: Vec<&str> = line.split("   ").collect(); 
-        first_col.push(split_line[0].parse::<i32>().unwrap());
-        second_col.push(split_line[1].parse::<i32>().unwrap());
+        let split_line: Vec<&str> = line.split_whitespace().collect(); 
+        if let (Some(first), Some(second)) = (split_line.get(0), split_line.get(1)) {
+            first_col.push(first.parse().unwrap_or_else(|_| panic!("Invalid integer: {}", first)));
+            second_col.push(second.parse().unwrap_or_else(|_| panic!("Invalid integer: {}", second)));
+        } else {
+            panic!("Line does not have two columns: {}", line);
+        }
     }
 
     first_col.sort();
     second_col.sort();
 
 
-    let mut sum: i32 = 0;
+    let sum: i32 = first_col
+        .iter()
+        .zip(&second_col)
+        .map(|(a, b)| (a - b).abs())
+        .sum();
 
-    for (i, _) in first_col.iter().enumerate() {
-        let diff = first_col[i] - second_col[i];
-        sum += diff.abs();
-    }
-
-    println!("{}", sum)
+    println!("{}", sum);
 }
 
 pub fn solve2(input: &String) {
