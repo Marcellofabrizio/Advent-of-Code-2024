e4388108aac61b69ee5a3d2dd36b5b225f8f89f1
Marcello Fabrizio <marcfabrizio10@gmail.com>
2024-12-03 23:43:09 -0300

    trying with a lexer

--- Rust/src/day3/mod.rs
+++ Rust/src/day3/mod.rs
@@ -1,46 +1,195 @@
 use regex::Regex;
 
 pub fn solve1(input: &String) {
-    let re = Regex::new(r"mul\(\s*(\d+)\s*,\s*(\d+)\s*\)").unwrap();
-    let sum: i32 = re.captures_iter(input)
-    .map(|caps| {
-        let op1 = caps.get(1).unwrap().as_str().parse::<i32>().unwrap_or_else(|_| {
-            panic!("Invalid integer for value 1: {}", caps.get(1).unwrap().as_str())
-        });
-        let op2 = caps.get(2).unwrap().as_str().parse::<i32>().unwrap_or_else(|_| {
-            panic!("Invalid integer for value 2: {}", caps.get(2).unwrap().as_str())
-        });
-        op1 * op2
-    })
-    .sum();
+    let mut iter = input.split("").into_iter().peekable();
+    let mut lexer: String = String::new();
+    let mut sum_enabled = true;
 
-println!("{}", sum);
+    while let Some(mut token) = iter.next() {
+        if token == "" {
+            continue;
+        };
 
-}
+        println!("Current: {}", token);
 
-pub fn solve2(input: &String) {
+        match token {
+            "d" => {
+                lexer = String::new();
+                lexer += token;
+                token = iter.next().unwrap();
+                if token == "o" {
+                    lexer += token;
+                    token = iter.next().unwrap();
+                    if token == "(" {
+                        lexer += token;
+                        token = iter.next().unwrap();
+                        if token == ")" {
+                            lexer += token;
+                        }
+                    }
+                }
 
-  let re = Regex::new(r"(mul\(\s*(\d+)\s*,\s*(\d+)\s*\)|don't()|do())").unwrap();
-  let mut sum_enabled = true;
-  let mut sum = 0;
-  for cap in re.captures_iter(input) {
-    if let Some(op) = cap.get(0) {
-        match op.as_str() {
-            "do" => sum_enabled = true,
-            "don't" => sum_enabled = false,
-            _ if sum_enabled => {
-                let op1 = cap.get(1).unwrap().as_str().parse::<i32>().unwrap_or_else(|_| {
-                    panic!("Invalid integer for value 1: {}", cap.get(1).unwrap().as_str())
-                });
-                let op2 = cap.get(2).unwrap().as_str().parse::<i32>().unwrap_or_else(|_| {
-                    panic!("Invalid integer for value 2: {}", cap.get(2).unwrap().as_str())
-                });
-                sum += op1 * op2;
+                if token == "n" {
+                    lexer += token;
+                    token = iter.next().unwrap();
+                    if token == "'" {
+                        lexer += token;
+                        token = iter.next().unwrap();
+                        if token == "t" {
+                            lexer += token;
+                            token = iter.next().unwrap();
+                            if token == "(" {
+                                lexer += token;
+                                token = iter.next().unwrap();
+                                if token == ")" {
+                                    lexer += token;
+                                }
+                            }
+                        }
+                    } else {
+                        continue;
+                    }
+                }
+
+                println!("lexer {}", lexer);
+
+                match lexer.as_str() {
+                    "don't()" => sum_enabled = false,
+                    "do()" => sum_enabled = true,
+                    _ => (),
+                }
+            }
+            "m" => {
+                if sum_enabled {
+                    lexer = String::new();
+                    lexer += token;
+
+                    token = iter.next().unwrap();
+
+                    if token == "u" {
+                        token = iter.next().unwrap();
+                    }
+                }
             }
             _ => {}
         }
     }
 }
 
-  println!("{}", sum);
+pub fn solve1_lexer(input: &String) {
+  let mut iter = input.split("").into_iter().peekable();
+  let mut lexer: String = String::new();
+  let mut sum_enabled = true;
+
+  while let Some(mut token) = iter.next() {
+      if token == "" {
+          continue;
+      };
+
+      println!("Current: {}", token);
+
+      match token {
+          "d" => {
+              lexer = String::new();
+              lexer += token;
+              token = iter.next().unwrap();
+              if token == "o" {
+                  lexer += token;
+                  token = iter.next().unwrap();
+                  if token == "(" {
+                      lexer += token;
+                      token = iter.next().unwrap();
+                      if token == ")" {
+                          lexer += token;
+                      }
+                  }
+              }
+
+              if token == "n" {
+                  lexer += token;
+                  token = iter.next().unwrap();
+                  if token == "'" {
+                      lexer += token;
+                      token = iter.next().unwrap();
+                      if token == "t" {
+                          lexer += token;
+                          token = iter.next().unwrap();
+                          if token == "(" {
+                              lexer += token;
+                              token = iter.next().unwrap();
+                              if token == ")" {
+                                  lexer += token;
+                              }
+                          }
+                      }
+                  } else {
+                      continue;
+                  }
+              }
+
+              println!("lexer {}", lexer);
+
+              match lexer.as_str() {
+                  "don't()" => sum_enabled = false,
+                  "do()" => sum_enabled = true,
+                  _ => (),
+              }
+          }
+          "m" => {
+              if sum_enabled {
+                  lexer = String::new();
+                  lexer += token;
+
+                  token = iter.next().unwrap();
+
+                  if token == "u" {
+                      token = iter.next().unwrap();
+                  }
+              }
+          }
+          _ => {}
+      }
+  }
+}
+
+pub fn solve2(input: &String) {
+    let re = Regex::new(r"(mul\(\s*(\d+)\s*,\s*(\d+)\s*\)|don't()|do())").unwrap();
+    let mut sum_enabled = true;
+    let mut sum = 0;
+    for cap in re.captures_iter(input) {
+        if let Some(op) = cap.get(0) {
+            match op.as_str() {
+                "do" => sum_enabled = true,
+                "don't" => sum_enabled = false,
+                _ if sum_enabled => {
+                    let op1 = cap
+                        .get(1)
+                        .unwrap()
+                        .as_str()
+                        .parse::<i32>()
+                        .unwrap_or_else(|_| {
+                            panic!(
+                                "Invalid integer for value 1: {}",
+                                cap.get(1).unwrap().as_str()
+                            )
+                        });
+                    let op2 = cap
+                        .get(2)
+                        .unwrap()
+                        .as_str()
+                        .parse::<i32>()
+                        .unwrap_or_else(|_| {
+                            panic!(
+                                "Invalid integer for value 2: {}",
+                                cap.get(2).unwrap().as_str()
+                            )
+                        });
+                    sum += op1 * op2;
+                }
+                _ => {}
+            }
+        }
+    }
+
+    println!("{}", sum);
 }
--- Rust/src/main.rs
+++ Rust/src/main.rs
@@ -4,6 +4,6 @@ mod day3;
 fn main() {
     println!("Solving AOC 2024\n");
     let input = fs::read_to_string("day3/data").expect("File not found");
-    day3::solve1(&input);
-    day3::solve2(&input);
+    day3::solve1_lexer(&input);
+    // day3::solve2(&input);
 }
